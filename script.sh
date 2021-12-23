3a276f4182456b65dd96beb4157e65413589cde8
Madhab Acharya <madhaba@rabbit.co.th>
2021-12-23 17:47:45 +0700

    Use buf.

--- script.sh
+++ script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
 # find and generate docs for all .json 
-find . -name "*.json" -print0 | while read -d $'\0' file
+find ./gen -name "*.json" -print0 | while read -d $'\0' file
 do
-    fname="$(basename -- ${file})"
-    fname_m="${fname%?????}" ## remove .json from filename 
-    redoc-cli bundle ${file} -o build/${fname_m}.html
+    fname="$(basename -- ${file})" # eg. bar.swagger.json
+    fname_html=$(echo ${fname} | sed "s/swagger.json/html/") #bar.html
+
+    if [! -d "./build"]; then
+        mkdir build
+    fi
+
+    redoc-cli bundle ${file} -o build/${fname_html}
+    echo ${fname_html}
 done
 
 tar -czvf build.tar.gz ./build && rm -rf build
